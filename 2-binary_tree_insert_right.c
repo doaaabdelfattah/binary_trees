427501547349f09e2071432373a90b0067da2f14
“doaaabdelfattah <doaa.abdalfattah@gmail.com>
2024-01-30 21:18:46 +0000

    “task2”

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,6 +11,8 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	binary_tree_t *new_node;
 	binary_tree_t *tmp;
 
+	if (parent == NULL)
+		return (NULL);
 	/* Dynamically create BT node */
 	new_node = binary_tree_node(parent, value);
 
